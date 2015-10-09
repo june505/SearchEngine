08aa360ef21ebb0e3c10a7e622ebce71df31a5f7
Hou Jun <932738410@qq.com>
2015-10-09 08:56:16 -0700

    bugs in process() is clear

--- Server/src/Task.cpp
+++ Server/src/Task.cpp
@@ -18,11 +18,16 @@ Task::Task(int servfd,string queryStr,Application & app,InetAddress &clientAddr,
  */
 void Task::process()
 {
-	map<string,double> query;						//存储查询词权重
-	calculateWeight(query);							//计算查询词权重
+	map<string,double> query;		//存储查询词权重
+	calculateWeight(query);			//计算查询词权重
+	if(query.size()==0)
+	{
+		cout<<"查询词为空"<<endl;
+		return;
+	}
 	map<string,double>::iterator it = query.begin();
-	cout<<it->first<<endl;
-	map<int,double> ret = index_.search(it->first);	//调用InvertIndex中的search()函数，返回对应单词的倒排列表
+	map<int,double> ret;
+	ret = index_.search(it->first);	//调用InvertIndex中的search()函数，返回对应单词的倒排列表
 	map<int,double>::iterator pos = ret.begin();	//用于遍历ret的迭代器
 	map<int,map<string,double> > common;			//存储交集容器
 	while(pos!=ret.end())							//遍历map<int,double> ret 生成第一个交集
@@ -40,20 +45,33 @@ void Task::process()
 	//取交集值
 	while(it!=query.end())
 	{
-		cout<<it->first<<endl;
-		ret.clear();
 		string word(it->first);
 		ret = index_.search(word);//查找下一个查询词
+		map<int,double>::iterator invalid = ret.begin();
+		if(-1==(invalid->first))
+		{
+
+			query.erase(it);
+			it++;
+			continue;
+		}
+		if(0==(invalid->first))
+			ret.erase(invalid);
 		map<int,map<string,double> >::iterator beg1 = common.begin();
 		map<int,map<string,double> >::iterator last1 = common.end();
 		map<int,double>::iterator beg2 = ret.begin();
 		map<int,double>::iterator last2 = ret.end();
-		common = intersection(&beg1,&last1,&beg2,&last2,word);
+		common = intersection(&beg1,&last1,&beg2,&last2,word);//交集
 		it++;
 	}
 	//求余弦值
+	if(common.size()==0)
+	{
+		cout<<"交集为空"<<endl;
+		return;
+	}
 	map<int,map<string,double> >::iterator iter = common.begin();
-	map<string,double>::iterator iter1;
+	map<string,double>::iterator iter1 = query.begin();
 	Document doc;
 	while(iter!=common.end())
 	{
@@ -61,7 +79,7 @@ void Task::process()
 		double cos;
 		while(iter1!=query.end())
 		{
-			fenzi = fenzi +(iter1->second)*(query[iter1->first]);
+			fenzi = fenzi +(iter1->second)*(query[iter1->first]);//分子 = 分子 +
 			part1 = part1 +(iter1->second)*(iter1->second);
 			part2 = part2 +(query[iter1->first])*(query[iter1->first]);
 			++iter1;
@@ -69,6 +87,11 @@ void Task::process()
 		fenmu = sqrt(part1)*sqrt(part2);
 		cos = fenzi/fenmu;
 		memset(&doc,0,sizeof(doc));
+		if(iter->first==0)
+		{
+			++iter;
+			continue;
+		}
 		doc.docid = iter->first;
 		doc.cosValue = cos;
 		pq_.push(doc);
@@ -91,7 +114,8 @@ vector<int> Task::getRanged()
 {
 	vector<int> vec;
 	int size = pq_.size();
-	for(int i=0;i<size;i++)
+	int limit = (size<10)?size:10;
+	for(int i=0;i<limit;i++)
 	{
 		vec.push_back(pq_.top().docid);
 		pq_.pop();
@@ -102,39 +126,29 @@ Task::~Task()
 {
 }
 
+
+//计算权重
 void Task::calculateWeight(map<string,double> & query)
 {
 	map<string,int> freq;
-	vector<string> words;//切词结果
-	//app_.statistics(queryStr_,freq);//不能这样
-	app_.cut(queryStr_,words);//切词
-	statistics(words,freq);//词频统计
+	//queryStr_ = "核武器，原子弹外星生物科技";
+	app_.statistics(queryStr_,freq);
 	map<string,int>::iterator iter = freq.begin();
 	while(iter!=freq.end())
 	{
-		cout<<"在calculateWeight中word:"<<iter->first<<endl;
-		query[iter->first] =(iter->second)*(idf_.idf(iter->first));// weiget = tf*idf
-		cout<<query[iter->first]<<endl;
+		double idfVal = idf_.idf(iter->first);
+		if(idfVal==0)//找不到该单词的idf值
+		{
+			freq.erase(iter);//若在idf中找不到该单词，则将其删除
+			iter++;
+			continue;
+		}
+		query[iter->first] =(iter->second)*idfVal;// weiget = tf*idf
 		iter++;
 	}
 }
 
-//词频统计
-void Task::statistics(vector<string> &words,map<string,int> &freq)
-{
-	vector<string>::iterator iter = words.begin();
-	map<string,int>::iterator pos;
-	while(iter!=words.end())
-	{
-		pos = freq.find(*iter);
-		if(pos!=freq.end())
-			freq[*iter]++;
-		else
-			freq.insert(pair<string,int>(*iter,1));
-	}
-}
-
-//取交集
+//取交集函数
 map<int,map<string,double> > Task::intersection(
 		map<int,map<string,double> >::iterator *beg1,
 		map<int,map<string,double> >::iterator *last1,
@@ -143,6 +157,7 @@ map<int,map<string,double> > Task::intersection(
 		string word)
 {
 	map<int,map<string,double> > result;
+	++*beg2;//去掉返回的莫名其妙的0:0
 	while(*beg1!=*last1 && *beg2!=*last2)
 	{
 		if((*beg1)->first<(*beg2)->first)
@@ -151,8 +166,8 @@ map<int,map<string,double> > Task::intersection(
 			++*beg2;
 		else
 		{
-			pair<int,map<string,double> > p1;
-			p1 = **beg1;
+			pair<int,map<string,double> > p1;	//common中docid与ret中docid相同的部分
+			p1 = **beg1;						//把common中迭代器指向的位置赋给p1	
 			pair<string,double> p2;
 			p2.first = word;
 			p2.second =(*beg2)->second;
@@ -161,4 +176,5 @@ map<int,map<string,double> > Task::intersection(
 			++*beg1,++*beg2;
 		}
 	}
+	return result;
 }
